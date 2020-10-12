5b67cd29c05ec920f710523174c351a1588ac765
ermiry <erick_salas@ermiry.com>
2020-10-12 06:06:40 -0500

    Added base input methods

--- tc/graphs.cpp
+++ tc/graphs.cpp
@@ -7,6 +7,58 @@
 
 #include <bits/stdc++.h>
 
+#pragma region input
+
+static void input_clean_stdin (void) {
+
+	int c = 0;
+	do {
+		c = getchar ();
+	} while (c != '\n' && c != EOF);
+
+}
+
+// returns a newly allocated c string
+static char *input_get_line (void) {
+
+	size_t lenmax = 128, len = lenmax;
+	char *line = (char *) malloc (lenmax), *linep = line;
+
+	if (line) {
+		int c = 0;
+
+		for (;;) {
+			c = fgetc (stdin);
+
+			if (c == EOF || c == '\n') break;
+
+			if (--len == 0) {
+				len = lenmax;
+				char * linen = (char *) realloc (linep, lenmax *= 2);
+
+				if(linen == NULL) {
+					free (linep);
+					return NULL;
+				}
+
+				line = linen + (line - linep);
+				linep = linen;
+			}
+
+			if ((*line++ = c) == '\n') break;
+		}
+
+		*line = '\0';
+	}
+
+	return linep;
+
+}
+
+#pragma endregion
+
+#pragma region graph
+
 static void path_print (int parent[], int s, int v);
 
 static void graph_bfs (std::vector <int> adj[], int adjlen, int start, int end) {
@@ -67,11 +119,21 @@ static void path_print (int parent[], int s, int v) {
 
 }
 
+#pragma endregion
+
 int main (int argc, char const **argv) {
 
 	int adjlen = 32;
-
 	std::vector <int> adj[adjlen];
+
+	int min = 1, max = 12;
+
+	input_clean_stdin ();
+	printf ("\nIngresa el archivo con informacion del grafo: ");
+	char *filename = input_get_line ();
+	if (filename) {
+		free (filename);
+	}
 
 	graph_add_edge (adj, 1,	2);
 	graph_add_edge (adj, 2,	3);
@@ -96,7 +158,35 @@ int main (int argc, char const **argv) {
 	graph_add_edge (adj, 11, 10);
 	graph_add_edge (adj, 12, 11);
 
-	graph_bfs (adj, adjlen, 3, 11);
+	int start = 0;
+	while (1) {
+		printf ("\nIngresa el nodo inicial (%d - %d): ", min, max);
+		scanf ("%d", &start);
+
+		if ((start < min) || (start > max)) {
+			printf ("\nEl valor de inicio %d es invalido, intenta de nuevo\n", start);
+		}
+
+		else {
+			break;
+		}
+	}
+
+	int end = 0;
+	while (1) {
+		printf ("Ingresa el nodo destino (%d - %d): ", min, max);
+		scanf ("%d", &end);
+
+		if ((end < min) || (end > max)) {
+			printf ("\nEl valor de destino %d es invalido, intenta de nuevo\n", end);
+		}
+
+		else {
+			break;
+		}
+	}
+
+	graph_bfs (adj, adjlen, start, end);
 
 	return 0;
 
